5f1d1d248c02731db06d41c0d3aca22dcb2f8b5e
Lee, Jaehyeok <dlwogur15@gmail.com>
2021-07-23 09:56:21 +0900

    2021 - 07 - 23 리액트 Menu Drawer 구현

--- src/Hompage/Navigation.jsx
+++ src/Hompage/Navigation.jsx
@@ -7,7 +7,12 @@ import { withStyles } from '@material-ui/core/styles';
 import MenuIcon from  '@material-ui/icons/Menu';
 import  IconButton  from '@material-ui/core/IconButton';
 import { Link as RouterLink } from 'react-router-dom';
-import Link from '@material-ui/core/Link'
+import Link from '@material-ui/core/Link';
+import Drawer from '@material-ui/core/Drawer';
+import MenuItem  from '@material-ui/core/MenuItem';
+import HomeIcon from '@material-ui/icons/Home';
+import MenuBookIcon from '@material-ui/icons/MenuBook';
+import CollectionsBookmarkIcon from '@material-ui/icons/CollectionsBookmark';
 
 
 
@@ -17,9 +22,7 @@ const styles = {
       flexGrow : 1,
   },
   MenuButton : {
-      right : '20px',
-      top : '15px',
-      position : 'fixed',
+      left : '20px',
   },
   LoginButton : {
       right : '120px',
@@ -30,23 +33,31 @@ const styles = {
      color : 'black',
      left : '80px',
      top : '15px',
-     position : 'fixed',
+     position : 'absolute',
   },
   navlink2 : {
     color : 'black',
     left : '160px',
     top : '15px',
-    position : 'fixed',
+    position : 'absolute',
  },
  navlink3 : {
   color : 'black',
   left : '240px',
   top : '15px',
-  position : 'fixed',
+  position : 'absolute',
 },
 }
 
 class Navigation extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      toggle : false,
+    };
+  }
+
+  handerDrawer = () => this.setState({ toggle : !this.state.toggle})
 
 
 
@@ -59,11 +70,11 @@ class Navigation extends Component {
           <div>
           <div className = {classes.root}>
             <Navbar bg="light">
-            <IconButton color = "inherit"
+            <IconButton color = "inherit" className = {classes.MenuButton}
                onClick = {this.handerDrawer}>
                    <MenuIcon />
                </IconButton>
-            <Container>
+    <Container>
     <Nav className="me-auto">
       <Link className = {classes.navlink1} component = {RouterLink} to = "/">Home</Link>
       <Link className = {classes.navlink2} component = {RouterLink} to = "/novel">무협</Link>
@@ -71,6 +82,17 @@ class Navigation extends Component {
     </Nav>
     </Container>
   </Navbar>
+              <Drawer open = {this.state.toggle}>
+                       <MenuItem onClick = {this.handerDrawer}>
+                       <HomeIcon /><Link component = {RouterLink} to ="/"> Home</Link>
+                       </MenuItem>
+                       <MenuItem onClick = {this.handerDrawer}>
+                       <MenuBookIcon /><Link component = {RouterLink} to ="/novel"> 소설</Link>
+                       </MenuItem>
+                       <MenuItem onClick = {this.handerDrawer}>
+                       <CollectionsBookmarkIcon /><Link component = {RouterLink} to ="/webtoon"> 웹툰</Link>
+                       </MenuItem>
+               </Drawer>
        </div>
        <div style = {{margin : 'auto', marginTop : '20px'}}>
                { this.props.children }
