4907118252d92692dfd67a4dbbefff7bac25b8ac
Lee, Jaehyeok <dlwogur15@gmail.com>
2021-07-25 15:57:18 +0900

    2021 - 07 - 25 ComponentDidUpdate를 통해 조회기능 구현

--- src/Hompage/Cardcontent.jsx
+++ src/Hompage/Cardcontent.jsx
@@ -5,6 +5,11 @@ import Button from 'react-bootstrap/Button';
 import StarRate from '../Book/StarRate';
 import Grid from '@material-ui/core/Grid';
 import './Card.css';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import TextField  from '@material-ui/core/TextField';
 
 
 
@@ -15,7 +20,9 @@ class Cardcontent extends React.Component {
         super(props);
         this.state = {
             novel_name : '',
-            novel_view : null,
+            novel_view : 0,
+            novel_rate : null,
+            dialog : false,
         }
     }
 
@@ -38,7 +45,7 @@ class Cardcontent extends React.Component {
             "content-type" : "application/json",
           },
           body : JSON.stringify(view),
-        })
+        }).then(alert("새로고침을 눌러주세요!"))
     }
     
 
@@ -46,43 +53,84 @@ class Cardcontent extends React.Component {
           this.setState({
             novel_view : this.state.novel_view + 1,
             novel_name : this.props.name,
-            ratestate : false,
         });
       }
 
-      componentDidUpdate() {
-        this.onCall();
+      
+
+      componentDidUpdate(prevProps,prevState) {
+        if(prevState.novel_view !== this.state.novel_view) {
+          this.onCall();
+        }
+      }
+
+
+
+      handleDialog = () => {
+        this.setState({ dialog : !this.state.dialog});
+      }
+
+      handleChange = (e) => {
+        this.setState({
+          [e.target.name] : e.target.value,
+        });
+      }
+
+      SubmitRate = () => {
+        const rate = {
+          novel_name : this.props.name,
+          novel_rate : this.state.novel_rate,
+        }
+        fetch("http://localhost:4000/novelrate",{
+            method:"post",
+            headers : {
+              "content-type" : "application/json",
+            },
+            body : JSON.stringify(rate),
+          }).then(this.handleDialog)
       }
 
 
     render() {
         const img = this.props.img;
-        const imgurl = "/assets/"+img+".jpg";
+        const imgurl = "/novel/"+img+".jpg";
 
         return (
           <div>
             <Card border="dark" style={{ marginBottom : '20px', marginLeft : '30px', marginRight : '30px'}}>
-  <Card.Header>{this.props.header}</Card.Header>
-  <Card.Body>
-  <Grid container>
-    <Grid item xs = {12} lg = {2}>
-    <img src = {imgurl} width = "130" alt = {img} onClick = {this.IncreaseView} />
-    <Card.Title>{this.props.name}</Card.Title>
-    </Grid>
-    <Grid item xs = {12} lg = {6}>
-    <Card.Text>
-     조회수 : {this.props.view}
-    </Card.Text>
-    </Grid>
-    <Grid item xs = {8} lg = {1}>
-    <Button variant="outline-primary">별점 주기</Button>
-    </Grid>
-    <Grid item xs = {4} lg = {3}>
-    <StarRate rate = {this.props.rate} />
-    </Grid>
-    </Grid>
-       </Card.Body>
-       </Card>
+              <Card.Header>{this.props.header}</Card.Header>
+                <Card.Body>
+                  <Grid container>
+                  <Grid item xs = {12} lg = {2}>
+                      <img src = {imgurl} width = "180" alt = {img} onClick = {this.IncreaseView} />
+                <Card.Title>{this.props.name}</Card.Title>
+                    </Grid>
+               <Grid item xs = {12} lg = {6}>
+                <Card.Text>
+                        <p>조회수 : {this.props.view}</p>
+                       </Card.Text>
+                       </Grid>
+                    <Grid item xs = {8} lg = {1}>
+                  <Button variant="outline-primary" onClick = {this.handleDialog}>별점주기</Button>
+                </Grid>
+                  <Grid item xs = {4} lg = {3}>
+                    <StarRate rate = {this.props.rate} />
+                    </Grid>
+                     </Grid>
+                      </Card.Body>
+                      </Card>
+                    <Dialog open = {this.state.dialog} onClose = { this.handledialog}>
+                    <DialogTitle>별점주기</DialogTitle>
+                    <DialogContent>
+                       <p>
+                      <TextField label = "별점(1~5)" type = "number" name = "novel_rate" onChange = {this.handleChange}></TextField>
+                      </p>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button variant = "outline-success" onClick = {this.SubmitRate}>평가하기</Button>
+                        <Button variant = "outline-dark"  onClick = {this.handleDialog}>닫기</Button>
+                    </DialogActions>
+                </Dialog>
        </div>
         )
     }
--- src/Hompage/Cardcontent2.jsx
+++ src/Hompage/Cardcontent2.jsx
@@ -5,6 +5,12 @@ import Button from 'react-bootstrap/Button';
 import StarRate from '../Book/StarRate';
 import Grid from '@material-ui/core/Grid';
 import './Card.css';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import TextField  from '@material-ui/core/TextField';
+
 
 
 
@@ -16,6 +22,8 @@ class Cardcontent2 extends React.Component {
         this.state = {
             webtoon_view : null,
             webtoon_name : '',
+            webtoon_rate : null,
+            dialog : false,
         };
     }
 
@@ -31,8 +39,42 @@ class Cardcontent2 extends React.Component {
             "content-type" : "application/json",
           },
           body : JSON.stringify(view),
-        })
+        }).then(alert("새로고침을 눌러주세요!"))
     }
+
+   
+
+    SubmitRate = () => {
+      const rate = {
+        webtoon_name : this.props.name,
+        webtoon_rate : this.state.webtoon_rate,
+      }
+      fetch("http://localhost:4000/webtoonrate",{
+          method:"post",
+          headers : {
+            "content-type" : "application/json",
+          },
+          body : JSON.stringify(rate),
+        }).then(this.handleDialog)
+    }
+
+    handleDialog = () => {
+      this.setState({ dialog : !this.state.dialog});
+    }
+
+    handleChange = (e) => {
+      this.setState({
+        [e.target.name] : e.target.value,
+      });
+    }
+
+    componentDidUpdate(prevProps,prevState) {
+      if(prevState.webtoon_view !== this.state.webtoon_view) {
+        this.onCall();
+      }
+    }
+
+   
     
 
       IncreaseView = () => {
@@ -42,39 +84,50 @@ class Cardcontent2 extends React.Component {
         });
       }
 
-      componentDidUpdate() {
-        this.onCall();
-      }
+
 
 
     render() {
         const img = this.props.img;
-        const imgurl = "/imgfile/"+img+".jpg";
+        const imgurl = "/webtoon/"+img+".jpg";
+        console.log(this.state.webtoon_rate)
 
         return (
           <div>
             <Card border="dark" style={{ marginBottom : '20px', marginLeft : '30px', marginRight : '30px'}}>
-  <Card.Header>{this.props.header}</Card.Header>
-  <Card.Body>
-  <Grid container>
-    <Grid item xs = {12} lg = {2}>
-    <img src = {imgurl} width = "200" alt = {img} onClick = {this.IncreaseView} />
-    <Card.Title>{this.props.name}</Card.Title>
-    </Grid>
-    <Grid item xs = {12} lg = {6}>
-    <Card.Text>
-     조회수 : {this.props.view}
-    </Card.Text>
-    </Grid>
-    <Grid item xs = {8} lg = {1}>
-    <Button variant="outline-primary">별점 주기</Button>
-    </Grid>
-    <Grid item xs = {4} lg = {3}>
-    <StarRate rate = {this.props.rate} />
-    </Grid>
-    </Grid>
-       </Card.Body>
-       </Card>
+               <Card.Header>{this.props.header}</Card.Header>
+               <Card.Body>
+               <Grid container>
+                 <Grid item xs = {12} lg = {2}>
+                  <img src = {imgurl} width = "180" alt = {img} onClick = {this.IncreaseView} />
+                   <Card.Title>{this.props.name}</Card.Title>
+                 </Grid>
+                 <Grid item xs = {12} lg = {6}>
+                    <Card.Text onClick = {this.IncreaseView}>
+                      <p>조회수 : {this.props.view}</p>
+                    </Card.Text>
+                  </Grid>
+                  <Grid item xs = {8} lg = {1}>
+                    <Button variant="outline-primary" onClick = {this.handleDialog}>별점주기</Button>
+                  </Grid>
+                  <Grid item xs = {4} lg = {3}>
+                     <StarRate rate = {this.props.rate} />
+                  </Grid>
+                   </Grid>
+                    </Card.Body>
+                           </Card>
+                  <Dialog open = {this.state.dialog} onClose = { this.handleDialog}>
+                    <DialogTitle>별점주기</DialogTitle>
+                    <DialogContent>
+                      <p>
+                        <TextField label = "별점(1~5)" type = "number" name = "webtoon_rate" onChange = {this.handleChange}></TextField>
+                      </p>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button variant = "outline-success" onClick = {this.SubmitRate}>평가하기</Button>
+                        <Button variant = "outline-dark"  onClick = {this.handleDialog}>닫기</Button>
+                    </DialogActions>
+                </Dialog>
        </div>
         )
     }
